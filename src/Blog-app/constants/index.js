c74760709b04fd6898163e3351bf4b01f86d6c9f
Ritik Rathour <99650054+ritikrathour@users.noreply.github.com>
2024-05-23 00:29:31 +0530

    Update index.js
    fix constant

--- src/Blog-app/constants/index.js
+++ src/Blog-app/constants/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-export const BaseURL = "http://localhost:7000/api/v1";
-=======
-export const BaseURL = "https://blog-app-backend-v3ca.onrender.com/api/v1";
->>>>>>> 5c1b1725b6fe2916e07bf4f478dc07b6f1ecc213
+ 
+export const BaseURL = "https://blog-app-backend-v3ca.onrender.com/api/v1"; 
 export const NavLinks = [
     {link:"Home",id:"001",path:"/"},
     {link:"Blog Posts",id:"002",path:'/blog-posts'},
